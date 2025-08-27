f4e88e7f3c2351dc99f739cafccbd2189532a453
Juan Toranzos <toranzos628@gmail.com>
2025-08-27 16:12:46 -0300

    maquete el footer, de manera simple

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavegacionComponent from "@/components/ui/Navegacion";
+import FooterComponent from "@/components/ui/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       >
         <NavegacionComponent />
         {children}
+        <FooterComponent/>
       </body>
     </html>
   );
--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const FooterComponent = () => {
   return (
-    <div>FooterComponent</div>
+    <div>
+        <footer className='border-t border-border bg-card/50 backdrop-blur-sm mt-8 p-2'>
+                <p className='text-center text-2xl'>Todos los derechos reservados 2025 &copy; CineReview</p>
+        </footer>
+    </div>
   )
 }
 
