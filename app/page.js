7d7ec0a60c766c78df78519010ea7e6dcbd60b2f
Juan Toranzos <toranzos628@gmail.com>
2025-08-28 20:39:52 -0300

    Maquete el page principal y cree un componente para unas cards

--- app/page.js
+++ app/page.js
@@ -1,9 +1,24 @@
+import CardsHome from "@/components/general/CardsHome";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center"></h1>
-     
+      <h1 className="text-center text-5xl font-bold m-5 p-2">
+        Descubre el mundo del <span className="text-fuchsia-800">cine</span>{" "}
+      </h1>
+      <p className="text-center text-2xl m-5 p-2">
+        Explora miles de peliculas, lee reseñas auténticas y comparte tu opinion <br/>
+        <span className="">con una comunidad apasionada por el cine.</span>
+      </p>
+      <div className="flex justify-center gap-4 m-5 p-2">
+      <Link className="bg-fuchsia-800 p-2 rounded w-[200px] text-center" href={"/"}>Comenzar ahora</ Link>
+      <Link className="p-2 border rounded  w-[200px] text-center" href={'/'}>Explorar Peliculas</Link>
+      </div>
+      <div className="m-5 p-2">
+        {/* Aquí puedes agregar el componente CardsHome */}
+        <CardsHome/>
+        </div>
     </div>
   );
 }
