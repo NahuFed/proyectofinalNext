a959ab130de5c6e217184f41e17f072fe7bee86e
emilianosala <emilianosala48@gmail.com>
2025-09-14 23:46:15 -0300

    feat(search): muestra SearchBar en /movies (UI)

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -1,21 +1,27 @@
-import MovieComponent from '@/components/general/MovieComponent'
-import { TrendingUp } from 'lucide-react'
+import MovieComponent from '@/components/general/MovieComponent';
+import { TrendingUp } from 'lucide-react';
+import SearchBar from '@/components/ui/SearchBar';
 
 const page = () => {
-  
   return (
     <div>
-        <h1 className='text-start text-4xl font-bold p-2'>Explorar peliculas</h1>
-        <p className='font-bold p-2'>Descubre tu proxima pelicula favorita </p>
+      <h1 className="text-start text-4xl font-bold p-2">Explorar peliculas</h1>
+      <p className="font-bold p-2">Descubre tu proxima pelicula favorita </p>
+      <div className="p-2">
+        <SearchBar />
+      </div>
+      <p className="text-2xl flex p-2 m-2 ">
+        <span>
+          <TrendingUp className="text-fuchsia-800 " />
+        </span>
+        Mejor calificadas
+      </p>
 
-        <p className='text-2xl flex p-2 m-2 '><span><TrendingUp className='text-fuchsia-800 ' /></span>Mejor calificadas</p>
-       
-        <div>
-          <MovieComponent />
-        </div>
-     
+      <div>
+        <MovieComponent />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
